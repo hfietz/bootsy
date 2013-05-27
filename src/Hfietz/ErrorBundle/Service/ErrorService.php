1f280259a4a29d5c2e3f19264cdd86ec248fb6b9
Hanno Fietz <hanno.fietz@econemon.com>
2013-05-27 16:00:56 +0200

    Added a new interface that allows the db service to poll other services for the location of specific db update scripts and implemented it for the error service.

--- src/Hfietz/ErrorBundle/Service/ErrorService.php
+++ src/Hfietz/ErrorBundle/Service/ErrorService.php
@@ -4,13 +4,21 @@ namespace Hfietz\ErrorBundle\Service;
 
 use Hfietz\DatabaseBundle\Service\DatabaseService;
 use Hfietz\DatabaseBundle\Service\DatabaseServiceAware;
+use Hfietz\DatabaseBundle\Service\DatabaseUpdateProvider;
 use Hfietz\ErrorBundle\Model\LoggedException;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-class ErrorService implements EventSubscriberInterface, DatabaseServiceAware
+class ErrorService implements EventSubscriberInterface, DatabaseServiceAware, DatabaseUpdateProvider
 {
+  /**
+   * @var KernelInterface
+   */
+  protected $kernel;
+
   /**
    * @var DatabaseService
    */
@@ -21,6 +29,11 @@ class ErrorService implements EventSubscriberInterface, DatabaseServiceAware
     return array(
       'kernel.exception' => array('onKernelException', 0),
     );
+  }
+
+  public function __construct(KernelInterface $kernel = NULL)
+  {
+    $this->kernel = $kernel;
   }
 
   public function onKernelException(GetResponseForExceptionEvent $e)
@@ -55,5 +68,19 @@ class ErrorService implements EventSubscriberInterface, DatabaseServiceAware
   function setDatabaseService(DatabaseService $databaseService = NULL)
   {
     $this->databaseService = $databaseService;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDbScriptPath()
+  {
+    // TODO: We want a reliable and framework-compliant way to determine the bundle path relative to the installation root
+    $fs = new Filesystem();
+    $reflection = new \ReflectionObject($this);
+    $path = realpath(dirname($reflection->getFileName()) . DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, array('..', 'Resources', 'db')));
+    $root = realpath($this->kernel->getRootDir() . '/..'); // KernelInterface::getRootDir always returns Unix-style paths
+
+    return $fs->makePathRelative($path, $root);
   }
 }
