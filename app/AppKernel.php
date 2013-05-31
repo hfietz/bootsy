0bf47b133e8c9cdf2b4cd763028475c0ed548a0e
Hanno Fietz <hanno.fietz@econemon.com>
2013-05-31 17:54:47 +0200

    Backport: Activated application bundle in kernel. (cherry picked from commit c51f61b)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
       new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
       new Econemon\Bootsy\DatabaseBundle\EconemonBootsyDatabaseBundle(),
       new Econemon\Bootsy\ErrorBundle\EconemonBootsyErrorBundle(),
+      new Econemon\Bootsy\ApplicationBundle\EconemonBootsyApplicationBundle(),
     );
 
     if (in_array($this->getEnvironment(), array('dev', 'test'))) {
