121d71a10ea58a312ade97d8544c5981bfae37e2
Hanno Fietz <hanno.fietz@econemon.com>
2013-06-11 11:31:56 +0200

    Added export, i. e., reverse mapping from object properties to column names. The reverse operation of hydrate. (cherry picked from commit e480229)

--- src/Econemon/Bootsy/DatabaseBundle/Service/Hydrator.php
+++ src/Econemon/Bootsy/DatabaseBundle/Service/Hydrator.php
@@ -7,15 +7,29 @@ class Hydrator
   public static function hydrate($object, $row, $mapping = array())
   {
     foreach ($row as $name => $value) {
-      $name = preg_replace_callback('/_([a-z])/', function ($matches) {
-        return strtoupper($matches[1]);
-      }, strtolower($name));
+      $name = self::getDefaultMappedColumnName($name);
       if (FALSE === self::mapTo($object, $name, $value) && array_key_exists($name, $mapping)) {
         self::mapTo($object, $mapping[$name], $value);
       }
     }
 
     return $object;
+  }
+
+  public static function export($object, $mapping = array())
+  {
+    $export = array();
+
+    foreach ($mapping as $column => $property) {
+      $getter = self::getGetterForName($property);
+      if (method_exists($object, $getter)) {
+        $export[$column] = call_user_func(array($object, $getter));
+      } else if (property_exists($object, $property)) {
+        $export[$column] = $object->{$property};
+      }
+    }
+
+    return $export;
   }
 
   public static function mapTo($object, $name, $value)
@@ -40,5 +54,23 @@ class Hydrator
   {
     // We assume camel-casing
     return 'set' . ucfirst($name);
+  }
+
+  public static function getGetterForName($name)
+  {
+    // We assume camel-casing
+    return 'get' . ucfirst($name);
+  }
+
+  /**
+   * @param $name
+   * @return mixed
+   */
+  public static function getDefaultMappedColumnName($name)
+  {
+    $name = preg_replace_callback('/_([a-z])/', function ($matches) {
+      return strtoupper($matches[1]);
+    }, strtolower($name));
+    return $name;
   }
 }
