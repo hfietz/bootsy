57d9e00210c9e9decc11b5a60e4b33ad631eed42
Hanno Fietz <hanno.fietz@econemon.com>
2013-06-17 21:48:09 +0200

    WIP: Customized the profile edit page. (cherry picked from commit 244ba4a)

--- src/Econemon/Bootsy/UserBundle/Form/ProfileFormType.php
+++ src/Econemon/Bootsy/UserBundle/Form/ProfileFormType.php
@@ -13,12 +13,33 @@ class ProfileFormType extends BaseForm
   {
     parent::buildForm($builder, $options);
 
-    $builder->add('name', 'text', array('required' => FALSE, 'label' => 'form.user.name', 'translation_domain' => 'bootsy_user'));
-    $builder->add('phone', 'text', array('required' => FALSE, 'label' => 'form.user.phone', 'translation_domain' => 'bootsy_user'));
+    // Stolen from ChangePasswordFormType
+    $builder->add('plainPassword', 'repeated', array(
+      'type' => 'password',
+      'options' => array('translation_domain' => 'FOSUserBundle'),
+      'first_options' => array('label' => 'form.new_password'),
+      'second_options' => array('label' => 'form.new_password_confirmation'),
+      'invalid_message' => 'fos_user.password.mismatch',
+    ));
+
+    $builder->add('name', 'text', $this->standard_options('form.user.name'));
+    $builder->add('phone', 'text', $this->standard_options('form.user.phone'));
+
+    $field_options = array_merge($this->standard_options('form.user.newEmail'), array('mapped' => false,));
+    $builder->add('newEmail', 'email', $field_options);
   }
 
   public function getName()
   {
     return self::FORM_NAME;
+  }
+
+  /**
+   * @param $label
+   * @return array
+   */
+  public function standard_options($label)
+  {
+    return array('required' => FALSE, 'label' => $label, 'translation_domain' => 'bootsy_user');
   }
 }
